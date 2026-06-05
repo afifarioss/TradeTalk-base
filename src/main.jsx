b38fae3e967bde43db0d845ff8d716c106fb8822
afifarioss <afif.peugeot@gmail.com>
2026-06-05 08:24:12 +0800

    Create src/main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import TradeTalk from './TradeTalk.jsx'
+import './index.css'   // optional - you can create this later for extra styles
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TradeTalk />
+  </React.StrictMode>,
+)
